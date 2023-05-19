ea7ceeb7dac5b1582c796c460cb2f7ac25bd69c3
TiagoNovaes <tiagousnovaes@gmail.com>
2023-05-19 13:41:16 -0300

    Feat: get url and visit url

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -10,10 +10,48 @@ export async function urlShorten(req, res) {
         const urlInfo = await db.query(`
         INSERT INTO urls ("userId", url, "shortUrl")
         VALUES ($1, $2, $3)
-        RETURNING id;`, [user.id, url, shortUrl]);
+        RETURNING id;`,
+            [user.id, url, shortUrl]);
 
-        res.status(201).send({ id: urlInfo.rows[0].id, shortUrl })
+        const body = {
+            id: urlInfo.rows[0].id,
+            shortUrl
+        }
+
+        res.status(201).send(body);
     } catch (err) {
         res.status(500).send(err.message);
     }
 };
+
+export async function getUrl(req, res) {
+    const { id } = req.params;
+
+    try {
+        const url = await db.query(`SELECT id, "shortUrl", url FROM urls WHERE id=$1;`, [id]);
+        if (!url.rowCount) return res.sendStatus(404);
+
+        res.status(200).send(url.rows[0]);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+export async function visitUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+        const visitUrl = await db.query(`
+        UPDATE urls 
+        SET "visitCount" = "visitCount"+1 
+        WHERE "shortUrl"=$1 
+        RETURNING url;`,
+            [shortUrl]);
+
+        if(!visitUrl.rowCount) return res.sendStatus(404);
+
+        res.redirect(visitUrl.rows[0].url);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validationSchema.middleware.js";
 import { urlSchema } from "../schemas/urls.schemas.js";
 import { validateToken } from "../middlewares/validationToken.middleware.js";
-import { urlShorten } from "../controllers/urls.controllers.js";
+import { getUrl, urlShorten, visitUrl } from "../controllers/urls.controllers.js";
 
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten", validateToken, validateSchema(urlSchema), urlShorten);
+urlsRouter.get("/urls/:id", getUrl);
+urlsRouter.get("/urls/open/:shortUrl", visitUrl);
 
 export default urlsRouter;
