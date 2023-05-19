b28334dfd5930d901f74d7f77a580b8267e8a3d3
TiagoNovaes <tiagousnovaes@gmail.com>
2023-05-19 15:03:30 -0300

    Fix: usersSchema confirmPassword

--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -4,7 +4,7 @@ export const signUpSchema = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
-    confirm_password: joi.string().valid(joi.ref('password')).required()
+    confirmPassword: joi.string().valid(joi.ref('password')).required()
 });
 
 export const signInSchema = joi.object({
